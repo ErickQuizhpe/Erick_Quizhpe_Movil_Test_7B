1e3ad57b38e0aa13f2d3f27b8c624d7a64238219
ErickQuizhpe <equizhpe2003@gmail.com>
2025-12-01 18:04:20 -0500

    elimina código innecesario

--- App.tsx
+++ App.tsx
@@ -1,50 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
-import { TOTAL_STEPS, useVehiculoForm } from './state/useVehiculoForm';
+import { StyleSheet, Text, View } from 'react-native';
 import { VehiculoInfoScreen } from './screens/VehiculoInfoScreen';
 import { VehiculoDetailsScreen } from './screens/VehiculoDetailsScreen';
 import { VehiculoSummaryScreen } from './screens/VehiculoSummaryScreen';
-import { RegisteredVehiclesScreen } from './screens/RegisteredVehiclesScreen';
+import { TOTAL_STEPS, useVehiculoForm } from './state/useVehiculoForm';
+import { SafeAreaView } from 'react-native';
 
-export default function App() {
+export default function App() { 
 
-const {
-  vehiculo, 
-  updateField, 
-  step, 
-  nextStep, 
-  prevStep, 
-  reset, 
-  registerVehiculo, 
-  registeredVehiculos, 
-  showList
-} = useVehiculoForm();
-
+  const { vehiculo, updateField, step, nextStep, reset } = useVehiculoForm();
+  
   const renderScreen = () => {
-    if (showList) {
-      return <RegisteredVehiclesScreen vehiculos={registeredVehiculos} onRegisterAnother={reset} />
-    }
-    
     if (step === 0) {
       return <VehiculoInfoScreen vehiculo={vehiculo} onChange={updateField} onNext={nextStep} />
     }
     if (step === 1) {
-      return <VehiculoDetailsScreen vehiculo={vehiculo} onChange={updateField} onNext={nextStep} onBack={prevStep} />
-    }
-    if (step === 2) {
-      return <VehiculoSummaryScreen vehiculo={vehiculo} onRegister={registerVehiculo} onBack={prevStep} />
+      return <VehiculoDetailsScreen vehiculo={vehiculo} onChange={updateField} onNext={nextStep} onBack={reset} />
     }
   }
-return (
+
+  return (
     <SafeAreaView style={styles.safeArea}>
       <StatusBar style="auto" />
       <View style={styles.container}>
-        <Text style={styles.title}>
-          {showList ? 'Vehículos Registrados' : 'Registro de un vehículo'}
-        </Text>
-        {!showList && (
-          <Text style={styles.steps}>Paso {step + 1} de {TOTAL_STEPS}</Text>
-        )}
+        <Text style={styles.title}>Formulario de registro</Text>
+        <Text style={styles.steps}>Paso {step + 1} de {TOTAL_STEPS}</Text>
         <View style={styles.card}>
           {renderScreen()}
         </View>
@@ -69,17 +49,16 @@ const styles = StyleSheet.create({
     fontWeight: '700',
     color: 'black',
   },
-    card:{
+  steps:{
+    fontSize: 14,
+    color: 'black',
+  },
+  card:{
     flex: 1,
     backgroundColor: 'white',
     borderRadius: 12,
     padding: 20,
     borderWidth: 1,
     borderColor: 'white',
-  },
-    steps:{
-    fontSize: 14,
-    color: 'black',
-  },
+  }
 });
-
